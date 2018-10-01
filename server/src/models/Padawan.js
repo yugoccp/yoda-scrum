4878e4c69cfbda651bd5005c8cb62fcdf7286c8e
Yugo Sakamoto <yugoccp@gmail.com>
2018-10-01 00:55:19 -0300

    Remove unused files

--- server/src/models/Padawan.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const padawanSchema = new Schema({
-  name: String,
-  username: String
-});
-
-module.exports = mongoose.model('Padawan', padawanSchema);
